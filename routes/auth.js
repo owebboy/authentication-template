d8e0e0cc0c3a016f187123c7e243a9d01b67be5e
Oliver Pope <opope@me.com>
2016-01-04 16:04:42 +0000

    lots of swags

--- routes/auth.js
+++ routes/auth.js
@@ -3,61 +3,157 @@ var passport = require('passport');
 var Account = require('../models/account');
 var router = express.Router();
 
+// ------------ //
+// REGISTRATION //
+// ------------ //
+
 router.get('/register', function(req, res) {
-  res.render('register', { active: 'register' });
+  res.render('auth/register', { active: 'register' });
 });
 
 router.post('/register', function(req, res, next) {
-  Account.register(new Account({
-    username: req.body.username,
-    email: req.body.email,
-    name: req.body.name
-  }), req.body.password, function(err, account) {
-    if (err) {
-      return res.render("register", {
-        info: "username already exists!",
-        active: 'register'
-      });
-    }
+  
+  // confirm passwords match
+  if (req.body.confirm === req.body.password) {
+    
+    // register account
+    
+    Account.register(new Account({
+      username: req.body.username,
+      email: req.body.email,
+      name: req.body.name
+    }), req.body.password, function(err, account) {
+      
+      // handle error
+      
+      if (err) {
+        return res.render("auth/register", {
+          info: "username already exists!",
+          active: 'register'
+        });
+      }
+      
+      // save user and authenticate
 
-    passport.authenticate('local')(req, res, function() {
-      req.session.save(function(err) {
-        if (err) {
-          return next(err);
-        }
-        res.redirect('/');
+      passport.authenticate('local')(req, res, function() {
+        req.session.save(function(err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect('/');
+        });
       });
     });
-  });
+    
+  } else {
+    
+    // return as error
+    return res.render("auth/register", {
+      info: "passwords do not match",
+      active: 'register'
+    });
+    
+  }
+  
 });
 
+// ----- //
+// LOGIN //
+// ----- //
 
 router.get('/login', function(req, res) {
-  res.render('login', {
+  res.render('auth/login', {
     user: req.user,
     active: 'login'
   });
 });
 
 router.post('/login', function(req, res, next) {
+  
+  // authenticate account
+  
   passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.render("login", { info: "username and password do not match!", active: 'login' }); }
+    
+    // handle error
+    if (err) return next(err);
+    
+    // if no user then return error
+    if (!user) { 
+      return res.render("auth/login", { 
+        info: "username and password do not match!", 
+        active: 'login' 
+      }); 
+    }
+    
+    // log user in
+    
     req.logIn(user, function(err) {
-      if (err) { return next(err); }
+      
+      // handle error
+      if (err) return next(err);
+      
       return res.redirect('/');
+      
     });
   })(req, res, next);
 });
 
+// ------ //
+// LOGOUT //
+// ------ //
+
 router.get('/logout', function(req, res, next) {
   req.logout();
+  
   req.session.save(function(err) {
-    if (err) {
-      return next(err);
-    }
+    
+    // handle error
+    
+    if (err) return next(err);
+    
     res.redirect('/');
+    
   });
+});
+
+// ------ //
+// DELETE //
+// ------ //
+
+router.get('/delete', function(req, res, next) {
+   Account.findOneAndRemove({ _id: req.user.id }, function (err, account) {
+     
+     // handle error
+     
+     if (err) return next(err);
+     
+     res.redirect('/');
+     
+   });
+});
+
+// ------ //
+// UPDATE //
+// ------ //
+
+router.get('/update', function(req, res, next) {
+   res.render('auth/update', { user: req.user });
+});
+
+router.post('/update', function(req, res, next) {
+   Account.findOneAndUpdate({ _id: req.user.id }, {
+    email: req.body.email,
+    name: req.body.name,
+    username: req.body.username
+   }, function(err, account) {
+     
+     // handle error
+     
+     if (err) return next(err);
+     
+     res.redirect('/update');
+     
+   });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Account = require('../models/account');
 
+// get home page
 router.get('/', function (req, res) {
     res.render('index', { user : req.user, title: 'Authentication Template', active: 'home' });
 });
 
+// get user list page
+router.get('/users', function (req, res) {
+    res.render('users', { user : req.user, active: 'users' });
+});
+
+// run test
 router.get('/ping', function(req, res){
     res.status(200).send("pong!");
 });
