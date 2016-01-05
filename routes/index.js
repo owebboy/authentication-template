338e91dc42465c798956bd31a669223fbdfae4c0
Oliver Pope <opope@me.com>
2016-01-04 22:47:13 -0500

    auth-template @ 2.0

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,28 @@ var Account = require('../models/account');
 
 // get home page
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user, title: 'Authentication Template', active: 'home' });
+  res.render('index', { user : req.user, title: 'Authentication Template', description: 'A simple template for use with express.js and mongoose.js. Made with bootstrap.css', active: 'home' });
 });
 
 // get user list page
 router.get('/users', function (req, res) {
-    res.render('users', { user : req.user, active: 'users' });
+  Account.find({}, function (err, accounts) {
+    if (err) return next(err);
+    res.render('users', { user : req.user, account: accounts, active: 'users' });
+  });
+});
+
+// get individual user
+router.get('/user/:id', function (req, res) {
+  Account.findOne({ _id: req.params.id }, function (err, accounts) {
+    if (err) return next(err);
+    res.render('user', { user : req.user, account: accounts, active: 'users' });
+  });
 });
 
 // run test
 router.get('/ping', function(req, res){
-    res.status(200).send("pong!");
+  res.status(200).send("pong!");
 });
 
 module.exports = router;
